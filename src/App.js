62ff439dc265b830fdbb878f74e6e59b0d6419bf
asnimansari <asnim96@gmail.com>
2019-12-11 22:09:24 +0530

    Shows the Hexdump

--- src/App.js
+++ src/App.js
@@ -2,56 +2,69 @@ import React from 'react';
 
 import './App.css';
 
-function Appp() {
+class Appp extends React.Component {
 
-  let fileReader;
 
-  const handleFileRead = e => {
-    console.log(fileReader.result)
+  constructor(props) {
+    super(props);
+    this.fileReader = null
+    this.state = {filecontent:[]}
+
   }
 
 
-  const handleFileChosen = file=>{
-    fileReader = new FileReader()
-    fileReader.onloadend = handleFileRead;
-    fileReader.readAsArrayBuffer(file);
+  handleFileRead = e => {
+
+
+    const typedArray = new Uint8Array(this.fileReader.result)
+    const untypedArrray = [];
+    const iii = typedArray.values();
+
+
+
+
+    while (true) {
+
+      const { value, done } = iii.next()
+      if (done){
+        break
+      }
+
+      const hexValue = value.toString(16)
+
+      untypedArrray.push(hexValue.length === 1 ? `0${hexValue}`: hexValue)
+
+
+    }
+
+    this.setState({
+      filecontent:untypedArrray,
+    });
+
+
   }
 
-  return <div>
-    <input type={"file"} id={"file"}  accept={".bin"} onChange={e=>handleFileChosen(e.target.files[0])}/>
-  </div>
+
+
+  handleFileChosen = file => {
+    this.fileReader = new FileReader()
+    this.fileReader.onloadend = this.handleFileRead;
+    this.fileReader.readAsArrayBuffer(file);
+  }
+
+
+  render() {
+    console.log(this.state.filecontent.map(each=> each.toString()))
+
+    return <div>
+      <input type={"file"} id={"file"} accept={".bin"} onChange={e => this.handleFileChosen(e.target.files[0])}/>
+      <br/>
+      {this.state.filecontent.map(each=> `${each} `)}
+
+    </div>
+  }
 }
 
-// class Appp extends React.Component{
-//
-//   constructor(){
-//     super();
-//     this.state = {color:"red"}
-//
-//   }
-//   handleFileRead  = e => {
-//     co
-//   }
-//
-//
-//   handleChosenfile = (file)=> {
-//   const  fileReader = new FileReader();
-//   fileReader.onloadend = handel
-//
-//
-//   }
-//
-//
-//
-//   render() {
-//     return (
-//         <div>
-//           <input type={"file"} id={"files"} name={"files[]" } multiple onChange={this.handleFileSelect} />
-//         </div>
-//     );
-//   }
-// }
-//
 
 
 export default Appp;
